0e5eac392ba26dcf83cc2bd8308bceb7bd4c1086
Auditya Dino Pratama <auditya.dino@w3ocean.com>
2016-06-01 15:11:55 +0700

    Added Responsive Table List Component

---
+++ addon/components/mdl-table-responsive-cell.js
@@ -0,0 +1,48 @@
+import Ember from 'ember';
+import MdlTableResponsive from './mdl-table-responsive';
+import layout from '../templates/components/mdl-table-responsive-cell';
+
+export default Ember.Component.extend({
+	layout,
+	/*
+		Set Tagname of Component wrapper as <tr> element
+	*/
+	tagName: 'tr',
+
+	/*
+		Populate cell's column with properties of passed object. Property names are gotten from parent table headers.
+		Provide exception handling when object has setter/getter or not.
+	*/
+	records: Ember.computed('content', 'headers', function() {
+		var self = this,
+			table = this.get('table'),
+			records = Ember.A();
+
+		if(!Ember.isEmpty(this.get('content')) && table.get('headers')){
+			table.get('headers').forEach(function(header){
+				if(typeof self.get('content').get === 'function'){
+					records.pushObject({id: header.label, content: self.get('content').get(header.id)});
+				}
+				else{
+					records.pushObject({id: header.label, content: self.get('content')[header.id]});
+				}
+			});
+		}
+
+    return records;
+  }),
+
+	/*
+		Connect cell with parent table
+	*/
+  table: Ember.computed(function() {
+    return this.nearestOfType(MdlTableResponsive);
+  }),
+
+  /*
+		Bubble click action to route
+  */
+  click(){
+		this.sendAction('action', this.get('content'));
+	}
+});
---
+++ addon/components/mdl-table-responsive.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import layout from '../templates/components/mdl-table-responsive';
+
+export default Ember.Component.extend({
+	layout,
+	/*
+		Set Wrapper Class Names
+	*/
+	classNames: ["mdl-responsive-table"]
+});
