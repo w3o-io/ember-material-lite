f4bb6b367440d54bf75d50cadf0f57225eb750bb
Auditya Dino Pratama <auditya.dino@w3ocean.com>
2016-06-15 10:11:44 +0700

    Fix speed-dial not working on child route and components

--- addon/components/mdl-speed-dial-item.js
+++ addon/components/mdl-speed-dial-item.js
@@ -1,22 +1,37 @@
 import Ember from 'ember';
 import layout from '../templates/components/mdl-speed-dial-item';
-import speedDialBehavior from '../extensions/mdl-speed-dial';
 
 export default Ember.Component.extend({
 	/*
 		Inject imported template handlebars to component usinglayout 
 	*/
   layout,
-
-  /*
-    Inject Speed Dial Behavior from external dependency
-  */
-  speedDialBehavior,
   
   /*
     Class Name for component wrapper
   */
   classNames: ["mdl-speed-dial__option"],
+
+  /*
+    Show FAB option tooltip
+  */
+  showFABTooltip(e) {
+    $(this).children('.mdl-speed-dial__tooltip--hidden').show();
+  },
+
+  /*
+    Hide FAB option tooltip
+  */
+  hideFABTooltip(e) {
+    $(this).children('.mdl-speed-dial__tooltip--hidden').hide();
+  },
+
+  /*
+    Apply jQuery behaviors on element insertion
+  */
+  didInsertElement(){
+    Ember.$('.mdl-speed-dial__option').hover(this.showFABTooltip, this.hideFABTooltip);
+  },
 
   /*
     Bubble Action on click
--- addon/components/mdl-speed-dial.js
+++ addon/components/mdl-speed-dial.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import layout from '../templates/components/mdl-speed-dial';
-import speedDialBehavior from '../extensions/mdl-speed-dial';
 
 export default Ember.Component.extend({
 	/*
@@ -9,12 +8,91 @@ export default Ember.Component.extend({
   layout,
 
   /*
-    Inject Speed Dial Behavior from external dependency
-  */
-  speedDialBehavior,
-
-  /*
     Class Name for component wrapper
   */
   classNames: ["mdl-speed-dial", "mdl-speed-dial--bottom-fixed"],
+
+  /*
+    Rotate button on hover if there is secondary icon
+  */
+  rotateElement(el, deg, duration) {
+    $(el).stop().animate({ rotation: deg }, {
+      duration: duration,
+      step: function(now, fx) {
+        $(this).css({ "transform": "rotate(" + now + "deg)" });
+      }
+    });
+  },
+
+  /*
+    Open FAB Options
+  */
+  openFAB(e) {
+    var $btn = $(this),
+      $speedDialOptions = $btn.siblings('.mdl-speed-dial__options'),
+      isSpeedDialOptionsHidden = $speedDialOptions.css('display') == 'none' ? true : false,
+      $primaryIcon = $btn.children('.mdl-speed-dial_main-fab-icon--primary'),
+      $secondaryIcon = $btn.children('.mdl-speed-dial_main-fab-icon--secondary'),
+      isPrimaryIconNull = $primaryIcon.length > 0 ? false : true,
+      isSecondaryIconNull = $secondaryIcon.length > 0 ? false : true,
+      $primaryIcon = isPrimaryIconNull ? $btn.children('.mdl-speed-dial_main-fab-icon') : $primaryIcon,
+      isPrimaryIconPlusSign = $primaryIcon.html() == 'add' ? true : false,
+      rotationDegrees = 360,
+      rotationSpeed = 300,
+      rotate = $btn.hasClass('mdl-speed-dial__main-fab--spin');
+
+      console.log('asd');
+
+    if (isSpeedDialOptionsHidden) {
+      $speedDialOptions.fadeIn('fast');
+
+      if (rotate) {
+        if (isSecondaryIconNull && isPrimaryIconPlusSign) {
+          rotationDegrees = 45;
+          rotationSpeed = 100;
+        }
+        
+        rotateElement($btn, rotationDegrees, rotationSpeed);
+
+        if (!isPrimaryIconNull && !isSecondaryIconNull) {
+          $primaryIcon.fadeOut('fast');
+          $secondaryIcon.fadeIn('fast');
+        }
+      }
+    }
+  },
+
+  /*
+    Close FAB options
+  */
+  closeFAB(e) {
+    var $btn = $(this).children('.mdl-speed-dial__main-fab'),
+      $primaryIcon = $btn.children('.mdl-speed-dial_main-fab-icon--primary') || $btn.children('.mdl-speed-dial_main-fab-icon'),
+      $secondaryIcon = $btn.children('.mdl-speed-dial_main-fab-icon--secondary'),
+      isPrimaryIconNull = $primaryIcon.length > 0 ? false : true,
+      isSecondaryIconNull = $secondaryIcon.length > 0 ? false : true,
+      $primaryIcon = isPrimaryIconNull ? $btn.children('.mdl-speed-dial_main-fab-icon') : $primaryIcon,
+      rotate = $btn.hasClass('mdl-speed-dial__main-fab--spin');
+
+    $(this).children('.mdl-speed-dial__options').fadeOut('fast');
+
+    if (rotate) {
+      rotateElement($btn, 0, 100);
+
+      if (!isPrimaryIconNull && !isSecondaryIconNull) {
+        $primaryIcon.fadeIn('fast');
+        $secondaryIcon.fadeOut('fast');
+      }
+    }
+  },
+
+  /*
+    Apply jQuery behaviors on element insertion
+  */
+  didInsertElement() {
+    Ember.$('.mdl-speed-dial__main-fab').mouseenter(this.openFAB);
+    Ember.$('.mdl-speed-dial').mouseleave(this.closeFAB);    
+    Ember.$('.mdl-speed-dial__tooltip--hidden').hide();
+    Ember.$('.mdl-speed-dial_main-fab-icon--secondary').hide();
+  }
 });
