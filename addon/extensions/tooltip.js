bd3aa98c74fc7f600433c0a1b02356c07ff6ad6d
Auditya Dino Pratama <auditya.dino@w3ocean.com>
2016-10-24 10:29:18 +0700

    - Fix event handler error caused by update on material design lite git

--- addon/extensions/tooltip.js
+++ addon/extensions/tooltip.js
@@ -13,7 +13,7 @@ window.MaterialTooltip.prototype.init = function() {
 
     if (this.forElement_) {
       this.boundMouseEnterHandler = this.handleMouseEnter_.bind(this);
-        this.boundMouseLeaveHandler = this.handleMouseLeave_.bind(this);
+        this.boundMouseLeaveHandler = this.hideTooltip_.bind(this);
         this.forElement_.addEventListener('mouseenter', this.boundMouseEnterHandler, false);
         this.forElement_.addEventListener('touchend', this.boundMouseEnterHandler, false);
         this.forElement_.addEventListener('mouseleave', this.boundMouseLeaveHandler, false);
